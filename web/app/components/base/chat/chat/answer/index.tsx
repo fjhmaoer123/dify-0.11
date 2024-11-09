978c2f839d93452a292aecc037a6db0ded5e4233
fjhmaoer123 <137021487+fjhmaoer123@users.noreply.github.com>
2024-11-09 21:28:36 +0800

    11-9

--- web/app/components/base/chat/chat/answer/index.tsx
+++ web/app/components/base/chat/chat/answer/index.tsx
@@ -22,6 +22,7 @@ import AnswerIcon from '@/app/components/base/answer-icon'
 import { ChevronRight } from '@/app/components/base/icons/src/vender/line/arrows'
 import cn from '@/utils/classnames'
 import { FileList } from '@/app/components/base/file-uploader'
+import LogoAvatar from '@/app/components/base/logo/logo-embedded-chat-avatar'
 
 type AnswerProps = {
   item: ChatItem
@@ -103,11 +104,11 @@ const Answer: FC<AnswerProps> = ({
   return (
     <div className='flex mb-2 last:mb-0'>
       <div className='shrink-0 relative w-10 h-10'>
-        {answerIcon || <AnswerIcon />}
-        {responding && (
-          <div className='absolute -top-[3px] -left-[3px] pl-[6px] flex items-center w-4 h-4 bg-white rounded-full shadow-xs border-[0.5px] border-gray-50'>
-            <LoadingAnim type='avatar' />
-          </div>
+      {
+          answerIcon || (
+            <div className='flex items-center justify-center w-full h-full rounded-full bg-white border-[0.5px] border-black/5 text-xl'>
+              <LogoAvatar />
+            </div>
         )}
       </div>
       <div className='chat-answer-container group grow w-0 ml-4' ref={containerRef}>
